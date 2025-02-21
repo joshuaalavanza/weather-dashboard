6c0d9ccf477784e07821cef31d4fa58fb5f14584
Joshua Alavanza <alavanzajoshua@gmail.com>
2025-02-21 02:39:44 -0500

    updated api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const WeatherApp = () => {
     const fetchWeather = async () => {
       setError(null);
       try {
-          const response = await fetch(`http://127.0.0.1:5000/weather?city=${city}`);
+          const response = await fetch(`https://weather-dashboard-backend-fyrl.onrender.com/weather?city=${city}`);
           const data = await response.json();
           if (response.ok) {
               setWeather(data);
